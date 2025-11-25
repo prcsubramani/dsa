0b15caf2481905d81acffd9aa735343b5961a8ae
Bhavani1711 <subbuam73@gmail.com>
2025-11-25 21:36:30 +0000

    edge cases included

--- two sum/two sum.java
+++ two sum/two sum.java
@@ -1,7 +1,11 @@
 // Leetcode Link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 
 class Solution {
-    public int[] twoSum(int[] numbers, int target) {        
+    public int[] twoSum(int[] numbers, int target) { 
+         // edge cases
+         if(numbers ==null||numbers.length < 2){
+              return new int[0];
+         }		  
          Map<Integer,Integer> indicesMap = new HashMap<>();
           for(int i=0;i<numbers.length;i++) {
               int diff = target - numbers[i];
