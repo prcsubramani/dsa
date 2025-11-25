1e5c24c1cca6e94df750a375ceb3e1bff846bb7d
Bhavani1711 <subbuam73@gmail.com>
2025-11-25 21:32:04 +0000

    leetcode two sum

---
+++ two sum/two sum.java
@@ -0,0 +1,15 @@
+// Leetcode Link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {        
+         Map<Integer,Integer> indicesMap = new HashMap<>();
+          for(int i=0;i<numbers.length;i++) {
+              int diff = target - numbers[i];
+              if (indicesMap.containsKey(diff)) {
+                  return new int[]{indicesMap.get(diff), i};
+              }
+              indicesMap.put(numbers[i],i);
+          }
+          return null;
+    }
+}
